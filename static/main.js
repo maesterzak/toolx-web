410c759a420e94573efa3c1c05132649e690d30f
Abubakar Zakari <65318935+maesterzak@users.noreply.github.com>
2022-01-31 02:13:37 +0100

    5 commit

--- static/main.js
+++ static/main.js
@@ -1,4 +1,18 @@
-function form(e){
+function form_d(e){
     e.preventDefault();
-    console.log('helloe', e)
-}
+    console.log('helloe')
+}// which button is selected algo or usd
+
+function selectedSubjectName() {
+    var radios = document.getElementsByName("test");
+  
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+        // do whatever you want with the checked radio
+        console.log(radios[i].value);
+  
+        // only one radio can be logically checked, don't check the rest
+        break;
+      }
+    }
+  }
